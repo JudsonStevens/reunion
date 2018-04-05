1170115a53fe9b40ce3dd8e32c50488a33b3970b
Judson Stevens <Judson.Stevens2gmail.com>
2018-04-05 11:55:59 -0600

    Completion of iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,13 @@
+require './lib/activity'
 class Reunion
+  attr_reader :activities
+  def initialize(location)
+    @activities = []
+    @location = location
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,25 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+
+  def setup
+    @r = Reunion.new("Aspen")
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, @r
+  end
+
+  def test_it_can_add_activities
+    activity = Activity.new("climbing")
+    @r.add_activity(activity)
+    expected = "climbing"
+    actual = @r.activities[0].activity_name
+
+    assert_equal expected, actual
+  end
+
 end
