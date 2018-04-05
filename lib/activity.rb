1d9c051379894cab47fb1da90d8e43cfea558d3e
Judson Stevens <Judson.Stevens2gmail.com>
2018-04-05 11:27:22 -0600

    Addition of activity methods and tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,19 @@
 class Activity
+  attr_reader :activity_name,
+              :participants
+
+  def initialize(activity_name)
+    @participants = Hash.new
+    @activity_name = activity_name
+  end
+
+  def add_participant(participant_name, paid)
+
+    @participants[participant_name] = paid
+  end
+
+  def evaluate_cost
+    @participants.values.inject(:+)
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,38 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @a = Activity.new("climbing")
+  end
+
+  def test_it_exists
+    assert_instance_of Activity, @a
+  end
+
+  def test_it_has_a_name
+    expected = "climbing"
+    actual = @a.activity_name
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_add_participants
+    @a.add_participant("Sally", 15)
+    expected = {"Sally" => 15}
+    actual = @a.participants
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_evaluate_cost
+    @a.add_participant("Sally", 15)
+    @a.add_participant("Bob", 20)
+    expected = 35
+    actual = @a.evaluate_cost
+
+    assert_equal expected, actual
+    
+  end
+
 end
